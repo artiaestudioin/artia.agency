fbaacf99291cfd14b8be350bfc46972996a83d81
artiaestudioin <artia.estudioin@gmail.com>
2026-05-03 23:47:41 -0500

    Update page.tsx

--- app/portal/[access_code]/page.tsx
+++ app/portal/[access_code]/page.tsx
@@ -66,7 +66,7 @@ export default async function ClientPortalPage({ params }: { params: Promise<{ a
       </div>
 
       <div style={{ maxWidth: 900, margin: '0 auto', padding: '32px 24px' }}>
-        <ImageGallery files={fileList} projectId={project.id} />
+        <ImageGallery files={fileList} projectId={project.id} accessCode={access_code.toUpperCase()} />
       </div>
 
       {/* Contacto */}
