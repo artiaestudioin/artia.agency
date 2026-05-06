b7b2a1726aa1f609592b016b0078783507dd9467
artia-studio <artia.estudioin@gmail.com>
2026-05-05 22:57:13 -0500

    casi me duermui

--- app/admin/(protected)/reportes/ReportesClient.tsx
+++ app/admin/(protected)/reportes/ReportesClient.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import { useState, useRef, useMemo } from 'react'
-import EmptyState from '@/components/EmptyState'
 import jsPDF from 'jspdf'
 import html2canvas from 'html2canvas'
 import Link from 'next/link'
